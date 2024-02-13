88ca67bc7fba16b414cdd46d7cbb2323c3729dbc
fadgadahababashirdev <fadgadahababashirdev@gmail.com>
2024-02-13 09:25:10 +0200

    cors installation

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require("express")
 const mongoose = require("mongoose")
 require("dotenv").config()
 const app = express();
+const cors = require("cors")
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
